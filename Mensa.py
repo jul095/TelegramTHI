f90adaf38e02080feacab56f7da774e06a680343
jus5656 <jus5656@thi.de>
2016-12-15 17:10:28 +0100

    the bot is running

--- Mensa.py
+++ Mensa.py
@@ -1,11 +1,12 @@
 import urllib2
 import telegram
+from telegram.ext import Updater, CommandHandler
 import re
 
-bot = telegram.Bot(token='278206043:AAH13RqZiG1AdqcUpTDNbtAB-N_bL9EGeR4')
+token = '278206043:AAH13RqZiG1AdqcUpTDNbtAB-N_bL9EGeR4'
+link = "http://www.werkswelt.de/?id=ingo"
 
-
-response = urllib2.urlopen("http://www.werkswelt.de/?id=ingo").read()
+response = urllib2.urlopen(link).read()
 
 foodReg = re.compile(r'Speiseplan.*', re.IGNORECASE)
 pattern = re.compile(r'<\/br>(.*?)<\/br>(.*?)<\/br>.*?<\/br>\s+<\/br>', re.IGNORECASE)
@@ -18,16 +19,28 @@ result = []
 for elem in food:
  
     firstelem = re.sub(r"<sup>.*?<\/sup>", "", elem[0])
-    secondelem = re.sub(r"&nbsp;","",elem[1])
-    secondelem = re.sub(r"\s{2,}","",secondelem)
+    secondelem = re.sub(r"&nbsp;", "", elem[1])
+    secondelem = re.sub(r"\s{2, }" , "", secondelem)
     result.append(firstelem + " | " + secondelem)
 
 
-result = "\n".join(result)
+# result = "\n".join(result)
 print result
 
-updates = bot.getUpdates()
+# here for telegram bot
 
-print(updates)
+bot = telegram.Bot(token)
+updater = Updater(token)
 
-# print bot.getMe()
+def start(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Hallo, hier ist ihr THI-BOT")
+def meal(bot, update):
+        for elem in result:
+            bot.sendMessage(chat_id=update.message.chat_id, text=elem)
+
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('meal', meal))
+
+updater.start_polling()
