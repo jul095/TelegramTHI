fa9970dff533d309e3dfc766f1a24936552b579d
jus5656 <jus5656@thi.de>
2016-12-19 17:03:05 +0100

    also getting data of tomorrow

--- Mensa.py
+++ Mensa.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import urllib2
+import urllib2, urllib
 import telegram
 from telegram.ext import Updater, CommandHandler
 import re
@@ -11,40 +11,54 @@ link = "http://www.werkswelt.de/?id=ingo"
 
 
 timestamp = 0
-result = []
 
+mensadata = []
+
+days = ["today", "tomorrow"]
 
 def getMensaData():
-    global result, timestamp
+    global mensadata, timestamp
 
     # getting data every blabla minutes
-    if (time.time() - timestamp) > (60 * 30):
+    print (time.time() - timestamp)
+    if (time.time() - timestamp) > (60 * 0.5):
 
         # open website
-        response = urllib2.urlopen(link).read()
+        for day in days:
+            if day == "today":
+                response = urllib2.urlopen(link).read()      
+            else:
+                value = ({'mybutton': 'vorwärts'})
+                data = urllib.urlencode(value)
+                req = urllib2.Request(link, data)
+                response = urllib2.urlopen(req).read()
+ 
+            result = []
 
-        result = []
+            dateReg = re.compile(r'<h4>(.+?)</h4>', re.IGNORECASE)
+            foodReg = re.compile(r'Speiseplan.*', re.IGNORECASE)
+            pattern = re.compile(r'<\/br>(.*?)<\/br>(.*?)<\/br>.*?<\/br>\s+<\/br>', re.IGNORECASE)
+            priceReg = re.compile(r'[\d,]+', re.IGNORECASE)
 
-        foodReg = re.compile(r'Speiseplan.*', re.IGNORECASE)
-        pattern = re.compile(r'<\/br>(.*?)<\/br>(.*?)<\/br>.*?<\/br>\s+<\/br>', re.IGNORECASE)
-        priceReg = re.compile(r'[\d,]+', re.IGNORECASE)
+            food = foodReg.findall(response)[1]
+            food = pattern.findall(food)
+            date = dateReg.findall(response)[0]
+            result.append(str(date))
 
-        food = foodReg.findall(response)[1]
-        #print response
-        # print food
-        food = pattern.findall(food)
+            i = 1
+            for elem in food:
+                firstelem = re.sub(r"<sup>.*?<\/sup>", "", elem[0])
+                price = priceReg.findall(elem[1])
+                result.append("*Essen " + str(i) + " (" + price[0] + "€)*: " + firstelem)
+                i = i + 1
 
-        i = 1
-        for elem in food:
-            firstelem = re.sub(r"<sup>.*?<\/sup>", "", elem[0])
-            price = priceReg.findall(elem[1])
-            result.append("*Essen " + str(i) + " (" + price[0] + "€)*: " + firstelem)
-            i = i + 1
+            result = "\n".join(result)
+            print result
+            mensadata.append([day, result])
+            timestamp = time.time()
 
-        result = "\n".join(result)
-        print result
-        timestamp = time.time()
-    return result
+    return mensadata            
+# end function
 
 # here for telegram bot
 
@@ -53,13 +67,17 @@ updater = Updater(token)
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Hallo, hier ist ihr THI-BOT")
-def meal(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=getMensaData(), parse_mode=telegram.ParseMode.MARKDOWN)
+def mealtoday(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=getMensaData()[0][1], parse_mode=telegram.ParseMode.MARKDOWN)
+def mealtomorrow(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=getMensaData()[1][1], parse_mode=telegram.ParseMode.MARKDOWN)
 
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('meal', meal))
+dispatcher.add_handler(CommandHandler('meal', mealtoday))
+dispatcher.add_handler(CommandHandler('mealtoday', mealtoday))
+dispatcher.add_handler(CommandHandler('mealtomorrow', mealtomorrow))
 
 updater.start_polling()
 
